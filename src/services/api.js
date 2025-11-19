b4878813f09858a0c5407b6c4bc27469d5d651a7
Shubham Shree <shubham.shree@buddyloan.com>
2025-11-19 23:54:09 +0530

    Contact Form Data Functionality Added

--- src/services/api.js
+++ src/services/api.js
@@ -5,17 +5,35 @@
 // export const addContact = (contact) =>
 //   axios.post(`${API_BASE_URL}/api/contact-us`, contact)
 
+// export const addContact = async (formData) => {
+//   const scriptURL =
+//     'https://script.google.com/macros/s/AKfycbx1prZW0zKHZRCxqgaeAMHgfiU6vxSQUdGLndqlt1FcshPG-Vk8o_TJYYQxIGg6qgTI/exec'
+
+//   const response = await fetch(scriptURL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     body: formData,
+//   })
+
+//   return response
+// }
+
 export const addContact = async (formData) => {
   const scriptURL =
     'https://script.google.com/macros/s/AKfycbx1prZW0zKHZRCxqgaeAMHgfiU6vxSQUdGLndqlt1FcshPG-Vk8o_TJYYQxIGg6qgTI/exec'
 
+  const encoded = new URLSearchParams(formData).toString()
+
   const response = await fetch(scriptURL, {
     method: 'POST',
+    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: formData,
+    body: encoded,
   })
 
-  return response // <-- optional
+  return response
 }
